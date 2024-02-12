c8fe08c17b5a13b7bc33b860720715fe47966b5e
Andrei Scafi <andreiscafi@hotmail.com>
2024-02-12 06:19:17 -0300

    Adding variables to javascript.

---
+++ script.js
@@ -0,0 +1,5 @@
+//Variables
+const input = document.getElementById('input');
+const output = document.getElementById('output');
+const convertBtn = document.getElementById('convert-btn');
+//End of Variables
