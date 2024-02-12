98b28bf2afcb0d2550ed5a19496663041cf71d74
Andrei Scafi <andreiscafi@hotmail.com>
2024-02-12 07:09:47 -0300

    Adding input checker and return fucntion result to output.

--- script.js
+++ script.js
@@ -1,41 +1,55 @@
 //Variables
-const input = document.getElementById('input');
+const container = document.querySelector('.container');
+const number = document.getElementById('number');
 const output = document.getElementById('output');
 const convertBtn = document.getElementById('convert-btn');
 //End of Variables
 
 // Roman Numeral Converter Function
-function romanNumeralConverter(input) {
+function RomanNumeralConverter(input) {
+    const inputInt = parseInt(input);
 
-    const roman = {
-        M: 1000,
-        CM: 900,
-        D: 500,
-        CD: 400,
-        C: 100,
-        XC: 90,
-        L: 50,
-        XL: 40,
-        X: 10,
-        IX: 9,
-        V: 5,
-        IV: 4,
-        I: 1
-    }
-
-    let result = '';
-
-    for (const num in roman) {
-        //console.log(roman[num])//number
-        //console.log(num)//key
-
-        while (input >= roman[num]) {
-            result += num;
-            input -= roman[num]
+    if (isNaN(inputInt) || !input) {
+        return 'Please enter a valid number';
+    } else if (input < 1) {
+        return 'Please enter a number greater than or equal to 1';
+    } else {
+        const roman = {
+            M: 1000,
+            CM: 900,
+            D: 500,
+            CD: 400,
+            C: 100,
+            XC: 90,
+            L: 50,
+            XL: 40,
+            X: 10,
+            IX: 9,
+            V: 5,
+            IV: 4,
+            I: 1
         }
+
+        let result = '';
+
+        for (const num in roman) {
+            //console.log(roman[num])//number
+            //console.log(num)//key
+
+            while (input >= roman[num]) {
+                result += num;
+                input -= roman[num]
+            }
+        }
+
+        return result
     }
 
-    return result
+
 }
 // End of Roman Numeral Converter Function
 
+convertBtn.addEventListener('click', () => {
+    output.textContent = RomanNumeralConverter(number.value);
+    container.classList.add('change');
+})
